cffc438d6e5706373eaa9b02fda16cc7863efee1
vladislav t <vt118@mail.ru>
2019-11-05 23:00:44 +0300

    finished animation and Smooth Scrollin for hidden menu

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3036,9 +3036,22 @@ $(function(){
         nextArrow: '<img class="slider-arrow__right"  src="assets/img/double-right.svg">'
     });
 
-        $(".header__hamburger").click(function(){
-            $(".header__menu").fadeToggle().toggleClass('header__menu--open');
+     //Smooth Scrolling Using Navigation Menu
+	$('a[href*="#"]').on('click', function(e){
+		$('html,body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 100
+		},500);
+		e.preventDefault();
+	});
+
+        $('.header__hamburger').on('click', function() {
             $(this).toggleClass('header__hamburger-span--active');
+            $('.header__menu').toggleClass('header__menu--open');
+            
+            $('.header__menu-link').on('click', function() {
+                $('.header__hamburger').removeClass('header__hamburger-span--active');
+                $('.header__menu').removeClass('header__menu--open');
+            }); 
         });
 
 });
